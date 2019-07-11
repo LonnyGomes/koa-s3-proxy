9499dc43203ec7683ca94dbeba4ca4b27cb1ae32
Lonny Gomes <code@lonnygomes.com>
2019-07-10 22:37:27 -0400

    Add basic koa server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-console.log('hello');
+import Koa from 'koa';
+
+const app = new Koa();
+
+const PORT = 3000;
+
+app.use(async function(ctx, next) {
+    try {
+        await next();
+    } catch (err) {
+        // some errors will have .status
+        // however this is not a guarantee
+        ctx.status = err.status || 500;
+        ctx.type = 'json';
+        ctx.body = {
+            status: 'failed',
+            message: err.message || 'Something Failed!',
+        };
+
+        // since we handled this manually we'll
+        // want to delegate to the regular app
+        // level error handling as well so that
+        // centralized still functions correctly.
+        ctx.app.emit('error', err, ctx);
+    }
+});
+
+app.use(async (ctx: any, next: any) => {
+    await next();
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
+
